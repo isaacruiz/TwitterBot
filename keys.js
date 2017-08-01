84ebbab31a98ace191a5d0c93efe3dd0eb9b08d8
isaacruiz <isaac.ruiz02@utrgv.edu>
2017-08-01 10:15:28 -0500

    Delete keys.js

--- keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-     consumer_key:         'kJaA1b7FySoHr4n6DLWSh2k1M',
-     consumer_secret:      'QdGl8MpRP9YYj0OFpo8w3gq18peEUQM2MVH21BADYywNp7ztmP',
-     access_token:         '770977908319932416-5z7PSSfZ85X4uCKzzzA1S1KqwHWt0Xt',
-     access_token_secret:  'XXj4scgPZrgJRzcXdiRwWx2g3jpgOaE1hMmSGXw5tWthF',
-}
