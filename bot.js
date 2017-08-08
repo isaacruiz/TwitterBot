30b337ade7bf1a0df7d54197fa0ccb16be4e7517
Isaaac Ruiz <isaac.ruiz02@utrgv.edu>
2017-08-08 10:04:43 -0500

    Complete twitter bot

--- bot.js
+++ bot.js
@@ -2,96 +2,99 @@ console.log('The bot is starting');
 var Twit = require('twit');
 var keys = require('./keys');
 var T = new Twit(keys);
+var boundary_words = require('./boundary_words');
 	
-var Canvas = require('canvas'), Image = Canvas.Image, canvas = new Canvas(200, 200), ctx = canvas.getContext('2d');
+var Canvas = require('canvas'), Image = Canvas.Image, canvas = new Canvas(300, 300), ctx = canvas.getContext('2d');
 
-/*
-function enumerate_boundary_words(n, k)
-{
-        function neighbors(p)
-	{
-                return [(p[0], p[1]+1), (p[0]+1, p[1]), (p[0]-1, p[1]), (p[0], p[1]-1)]
-	}
-
-        function dir(p1, p2)
-	{
-                return (p2[0]-p1[0], p2[1]-p1[1])
-	}
-
-        path = [(0, 0)]
-        function recurse()
-	{
-                if (len(path) > n)
-                        return;
-                if (len(path) == n)
-				{
-                        if (!((0, 0) in neighbors(path[-1])))
-                                return;
-                        // Convert to a boundary word and yield it
-		
-                        word = (map(lambda i: vec2dir[dir(path[i], path[i+1])], xrange(len(path)-1)) +
-                                [vec2dir[dir(path[-1], (0, 0))]])
-                        if is_polyomino(word) and min(path) == (0, 0):
-                                yield word
-                }
-				head = path[-1]
-                for new in [(head[0], head[1]+1), (head[0]+1, head[1]), (head[0]-1, head[1]), (head[0], head[1]-1)]:
-                        if not (new in path): # Check for self-intersection
-                                path.append(new)
-                                for w in recurse():
-                                        yield w
-                                path.pop()
-        for w in recurse():
-                yield w
-	}
-} 
-*/
-
-var tweet_num = 1;
+var tweet_num = 4;
 postTweet();
-setInterval(postTweet, 1000 * 30);
-function postTweet(){
+setInterval(postTweet, 1000 * 60 * 15);
 
+function postTweet()
+{
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.fillStyle = "#ffffff";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+	var unit = 30;
+	var canvasSize = 10;
+	var boundary_box = bounding_box(boundary_words[tweet_num]);
+	var polyWidth = boundary_box[0] - boundary_box[1];
+	var polyHeight = boundary_box[2] - boundary_box[3];
+	var xInit = Math.trunc((canvasSize - polyWidth)/2);
+	var yInit =  Math.trunc((canvasSize - polyHeight)/2 - boundary_box[3]);
+	boundary_word_to_path(ctx, boundary_words[tweet_num])
+	function bounding_box(W)
+	{
+		var xMax = 0;
+		var yMax = 0;
+		var xMin = 0;
+		var yMin = 0;
+		var x = 0;
+		var y = 0;
+		
+		for(var i = 0; i < W.length; ++i)
+		{
+			if (W[i] == 'n')
+				y--;
+			else if (W[i] == 'e')
+				x++;
+			else if (W[i] == 'w')
+				x--;
+			else
+				y++;
+				
+			if(x > xMax)
+				xMax = x;
+			if(x < xMin)
+				xMin = x;
+			if(y > yMax)
+				yMax = y;
+			if(y < yMin)
+				yMin = y;
+		}
+		return [xMax, xMin, yMax, yMin];
+	}
 	function boundary_word_to_path(ctx, W)
 	{
+		
 		ctx.strokeStyle = 'rgba(0,0,0,10)';
 		ctx.beginPath();
-		var x = 80
-		var y = 80
+		var x = xInit * unit;
+		var y = yInit * unit;
+		
 		ctx.lineTo(x, y);
 		for (var i = 0; i < W.length; ++i)
 		{
-			if (W[i] == 'U')
-				y -= 30;
-			else if (W[i] == 'R')
-				x += 30;
-			else if (W[i] == 'L')
-				x -= 30;
+			if (W[i] == 'n')
+				y -= unit;
+			else if (W[i] == 'e')
+				x += unit;
+			else if (W[i] == 'w')
+				x -= unit;
 			else
-				y += 30;
+				y += unit;
 			ctx.lineTo(x, y);
 		}			
 		ctx.stroke();
 	}
 
-	boundary_word_to_path(ctx, "RDRRDRDLDLULDLULURUU")
-
 	T.post('media/upload', { media_data: canvas.toBuffer().toString('base64') }, uploaded);
 
-		function uploaded(err, data, response)
+	function uploaded(err, data, response)
+	{
+		var id = data.media_id_string;
+
+		var tweet =
 		{
-			var id = data.media_id_string;
-
-			var tweet =
-			{
-			status: 'Tweet number ' + tweet_num,
-			media_ids: [id]
-			}
-			T.post('statuses/update', tweet, tweeted);
+		status: "Polyomino Number: " + tweet_num + "\nBoundary Word: " + boundary_words[tweet_num - 1] + "\nBoundary Length: " + boundary_words[tweet_num - 1].length,
+		media_ids: [id]
 		}
+		T.post('statuses/update', tweet, tweeted);
+	}
 
-		function tweeted(err, data, response){
+	function tweeted(err, data, response)
+	{
 		console.log('Tweeted');
-		}
+	}
 	tweet_num++;
 }
