0447beb0af4176025ca6121f8d3f3c97e3b37535
Isaaac Ruiz <isaac.ruiz02@utrgv.edu>
2017-08-10 22:44:15 -0500

    Added auto sizing of canvas

--- bot.js
+++ bot.js
@@ -4,24 +4,34 @@ var keys = require('./keys');
 var T = new Twit(keys);
 var boundary_words = require('./boundary_words');
 	
-var Canvas = require('canvas'), Image = Canvas.Image, canvas = new Canvas(300, 300), ctx = canvas.getContext('2d');
 
-var tweet_num = 4;
+var tweet_num = 2500;
 postTweet();
-setInterval(postTweet, 1000 * 60 * 15);
+setInterval(postTweet, 1000 * 60 * 30);
 
 function postTweet()
 {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.fillStyle = "#ffffff";
-    ctx.fillRect(0,0,canvas.width,canvas.height);
+	
 	var unit = 30;
-	var canvasSize = 10;
 	var boundary_box = bounding_box(boundary_words[tweet_num]);
 	var polyWidth = boundary_box[0] - boundary_box[1];
 	var polyHeight = boundary_box[2] - boundary_box[3];
-	var xInit = Math.trunc((canvasSize - polyWidth)/2);
-	var yInit =  Math.trunc((canvasSize - polyHeight)/2 - boundary_box[3]);
+	var canvasWidth = polyWidth + 2;
+	var canvasHeight = polyHeight + 2;
+/* 	console.log("Canvas width: " + canvasWidth);
+	console.log("Canvas hegith: " + canvasHeight);
+	console.log("Poly width: " + polyWidth);
+	console.log("Poly height: " + polyHeight); */
+	var Canvas = require('canvas');
+	var	Image = Canvas.Image;
+	var canvas = new Canvas(canvasWidth * unit, canvasHeight * unit);
+	var	ctx = canvas.getContext('2d');
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.fillStyle = "#FFFFFF";
+	ctx.lineCap = "square"; 
+	ctx.fillRect(0,0, canvas.width, canvas.height);
+	var xInit = 1;
+	var yInit =  1 - boundary_box[3];
 	boundary_word_to_path(ctx, boundary_words[tweet_num])
 	function bounding_box(W)
 	{
@@ -57,7 +67,7 @@ function postTweet()
 	function boundary_word_to_path(ctx, W)
 	{
 		
-		ctx.strokeStyle = 'rgba(0,0,0,10)';
+		ctx.strokeStyle = 'rgba(0,0,0,100)';
 		ctx.beginPath();
 		var x = xInit * unit;
 		var y = yInit * unit;
@@ -74,8 +84,11 @@ function postTweet()
 			else
 				y += unit;
 			ctx.lineTo(x, y);
-		}			
+		}
+		ctx.lineWidth=10;
 		ctx.stroke();
+		ctx.fillStyle="#FF0000";
+		ctx.fill();
 	}
 
 	T.post('media/upload', { media_data: canvas.toBuffer().toString('base64') }, uploaded);
